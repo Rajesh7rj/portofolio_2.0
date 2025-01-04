fd339b6f5526c28574cece1f42d2138e551c9139
NITIN <rajeshjanyani7@gmail.com>
2025-01-04 23:11:51 +0530

    build error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  basePath: '/portofolio_2.0',
-  assetPrefix: '/portofolio_2.0/',
   eslint: {
     ignoreDuringBuilds: true,
   },
