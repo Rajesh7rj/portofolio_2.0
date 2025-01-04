59a73de0ae85bb205fbdafa3d8042ce8a515b840
NITIN <rajeshjanyani7@gmail.com>
2025-01-04 22:13:15 +0530

    build fixes

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const nextConfig = {
   basePath: '/portofolio_2.0',
   assetPrefix: '/portofolio_2.0/',
   rules: {
-    quotes: ["error", "single"], // Use single quotes
-    indent: ["error", 2], // Enforce 2 spaces for indentation
-    semi: ["error", "always"], // Require semicolons
+    // Disable specific rules
+    quotes: "off", // Disable enforcement of single/double quotes
+    indent: "off", // Disable indentation rules
+    semi: "off", // Disable semicolon rules
+
+    // Or, you can relax the rule level
+    quotes: ["warn", "single"], // Warn instead of error for quotes
   },
 }
 
